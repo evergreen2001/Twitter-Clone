95216b262a3c1a898cf3152d46319fd28a418469
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-11 17:02:58 +0100

    adding firebase tools

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDHbrQ2eaRY3BlcwZBL7he9Ntqh1QwRE1g",
+    authDomain: "todo-app-2bd4a.firebaseapp.com",
+    databaseURL: "https://todo-app-2bd4a.firebaseio.com",
+    projectId: "todo-app-2bd4a",
+    storageBucket: "todo-app-2bd4a.appspot.com",
+    messagingSenderId: "884213506436",
+    appId: "1:884213506436:web:d5258fcb81c490ebf3bdd1",
+    measurementId: "G-FWLKFTDLGJ"
+  };
+
+   const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+   const db = firebaseApp.firedtore()
+
+   export default db;
